1a46e5f2df8cfff45634980c5fc5deb941ae073b
Bruno Amorim <119983682+ToioCorrea@users.noreply.github.com>
2023-03-05 10:00:09 -0300

    initial commit

---
+++ primeiro.c
@@ -0,0 +1,17 @@
+//Primeiro programa
+
+//EXPLICAÇÃO AO DECORRER DO PROGRAMA USANDO O COMÉTARIO // OU /*
+
+/*Escreva um programa que escreva na tela "Olá Toio"*/
+
+#include <stdio.h>
+//include =  incluir ; sendo assim incluimos a biblioteca padrão stdio.h(ela contém os comando em linguagem C)
+int main(){
+//int main = usado para iniciar o programa  
+printf("Olá Toio");
+// printf = comando que escre uma mensagem na tela de saída
+//sempre terminamos a linha de comando com ";"
+//fica entre parenteses o texto para impressão e a expressão
+return 0;
+//return 0 = informa se esta tudo ok
+}
