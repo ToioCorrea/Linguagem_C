254b318294e97074c7b0a7943baab18bec12baff
Bruno Amorim <119983682+ToioCorrea@users.noreply.github.com>
2023-03-06 17:00:14 -0300

    exercicio 7

--- exercicios básicos/exercicio7.c
+++ exercicios básicos/exercicio7.c
@@ -15,3 +15,11 @@ printf("O raio é \'%2.f\' e sua área é %2.f", raio, area);
 
 return 0;
 }
+
+/*Comando “const”
+
+Sintaxe: const <tipo_de_dado> < nome_da_constante > = <valor>;
+- Declara uma constante para um tipo de dado específico;
+- Usa espaço na memória RAM;
+- O programa não compila se houver no código novo comando de atribuição para a constante (assignment of read-only variable).
+*/
